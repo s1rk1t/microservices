b16cbdf1784f9abca942580731c67f5bd8f0b87f
s1rk1t <37907871+s1rk1t@users.noreply.github.com>
2022-05-22 16:42:59 -0400

    config server upload

---
+++ configserver/src/main/java/com/p0s1t/configserver/ConfigserverApplication.java
@@ -0,0 +1,16 @@
+package com.p0s1t.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableConfigServer
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
